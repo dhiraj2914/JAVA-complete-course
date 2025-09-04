ec7ec5c15ffb3ca6603d7a565540452e4527ecc7
B.Dhiraj Gururam <dhirajgururam@gmail.com>
2025-09-04 11:28:28 +0530

    Add files via upload
    Basic print line function with single-multi line comments

---
+++ basic.java
@@ -0,0 +1,21 @@
+public class basic {
+    public static void main(String[] args){
+        // This is for single line comments.
+        /* this
+        is
+        for
+        multi
+        line
+        comments
+
+
+         */
+        System.out.print("My Name is Dhiraj\n");
+        System.out.println("This is my first Java program");
+        System.out.println("I like cricket");
+
+    }
+
+
+
+}
