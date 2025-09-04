1cf9d09c804c8e5871baef99a4fdaf1a2dc7cc31
B.Dhiraj Gururam <dhirajgururam@gmail.com>
2025-09-04 11:43:25 +0530

    diff types of variables used in java

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args){
+        boolean schoolStudent = true;
+        String name = "Dhiraj";
+        char grade = 'A';
+        double gpa = 9.8;
+        System.out.println(name);
+        System.out.println("Your grade is " + grade);
+        System.out.println("Your gpa is " + gpa);
+        if(schoolStudent){
+            System.out.println("Your enrolled");
+        }else{
+            System.out.println("Your not a student");
+        }
+
+
+
+
+    }
+}
